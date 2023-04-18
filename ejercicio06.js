9afec47195a472a005ecdfae0028fe69f10031d4
MiguelFloresVillarroel420 <116700329+destroyer420vivalamariguana@users.noreply.github.com>
2023-04-18 14:04:47 -0500

    Add files via upload

---
+++ ejercicio06.js
@@ -0,0 +1,11 @@
+let nombreCompleto = "Juan Perez";
+let nombrecompleto = "Carlos Lara";
+console.log( nombreCompleto);
+console.log( nombreCompleto);
+
+let alnombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+
+
+let ruptura = 10;
